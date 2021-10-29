cc83348585c91cb8ad8dfa97d77768739f98d8b8
DjNik <nikitos1235@gmail.com>
2021-10-29 09:02:40 +0300

    Added Cesar cipher

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+std::string CesarEncryptor(std::string, int);
+int main() {
+    std::string unencrypted_text;
+    int key = 0;
+    std::cout << "Enter some string and key!" << std::endl;
+    std::cin >> unencrypted_text >> key;
+    std::cout << "Cesar cipher: "  << CesarEncryptor(unencrypted_text, key);
+    return 0;
+}
+
+std::string CesarEncryptor(std::string text, int key) {
+    for (char & i : text) {
+        i += key;
+    }
+    return text;
+}
